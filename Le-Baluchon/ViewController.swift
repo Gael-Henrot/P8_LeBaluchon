147d9ec5466a7a8dcb6d005cba243b7020a70bc4
HENROT Gaël <gael.henrot@expleogroup.com>
2021-07-19 10:33:11 +0200

    Initial Commit

---
+++ Le-Baluchon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Le-Baluchon
+//
+//  Created by Gaël HENROT on 19/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
